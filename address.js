890c14100f4b88e21e9e0650174a08a1b4f8f793
nmattise <n.mattise@gmail.com>
2014-09-22 15:05:04 -0400

    uses ngAutocomplete to get google autocomplete geocoded data, not figured out how to get the map to refresh or zoom to location

---
+++ address.js
@@ -0,0 +1,54 @@
+{
+    "address_components": [{
+        "long_name": "2216",
+        "short_name": "2216",
+        "types": ["street_number"]
+    }, {
+        "long_name": "Library Ln",
+        "short_name": "Library Ln",
+        "types": ["route"]
+    }, {
+        "long_name": "College Park",
+        "short_name": "College Park",
+        "types": ["locality", "political"]
+    }, {
+        "long_name": "MD",
+        "short_name": "MD",
+        "types": ["administrative_area_level_1", "political"]
+    }, {
+        "long_name": "United States",
+        "short_name": "US",
+        "types": ["country", "political"]
+    }, {
+        "long_name": "20742",
+        "short_name": "20742",
+        "types": ["postal_code"]
+    }, {
+        "long_name": "0001",
+        "short_name": "0001",
+        "types": []
+    }],
+    "adr_address": "<span class=\"street-address\">2216 Library Ln</span>, <span class=\"locality\">College Park</span>, <span class=\"region\">MD</span> <span class=\"postal-code\">20742</span>, <span class=\"country-name\">United States</span>",
+    "formatted_address": "2216 Library Ln, College Park, MD, United States",
+    "formatted_phone_number": "(301) 405-6319",
+    "geometry": {
+        "location": {
+            "k": 38.98813,
+            "B": -76.941553
+        }
+    },
+    "icon": "http://maps.gstatic.com/mapfiles/place_api/icons/library-71.png",
+    "id": "88421565a9a55b679538bab14e7194f2082ed638",
+    "international_phone_number": "+1 301-405-6319",
+    "name": "Hornbake Library",
+    "place_id": "ChIJVVUVAZjGt4kReHROsUXRnpI",
+    "reference": "CnRqAAAAComWktioZU2oMbMzRj-2fXbv7zN-yW1PQTBQsai4mn-NBlP9PQ7ipMozil1H4giFJgN1EUk9jT1v3mjLqEnBHknOM4Dj_KK5Mpb4DPfPmyP1DUPEue4FpcktUHt72U_DI27MCR5mqftXhbwin5sehBIQJ6oSRcjOgbLZQ7bWEEq59RoUcVRwCp9aXvH604RFi5rC6Yo332E",
+    "scope": "GOOGLE",
+    "types": ["library", "establishment"],
+    "url": "https://plus.google.com/105998508460665799585/about?hl=en-US",
+    "utc_offset": -240,
+    "vicinity": "2216 Library Ln, College Park",
+    "website": "http://www.lib.umd.edu/",
+    "html_attributions": [],
+    "tz": "GMT-0400"
+}
