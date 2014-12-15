b17fb8968398e5e573dca3fa8a241c596dec4b47
nmattise <n.mattise@gmail.com>
2014-12-15 15:42:11 -0500

    groud loop

--- testpoly.js
+++ testpoly.js
@@ -23,33 +23,23 @@ function createGround(bldgFootprint, distanceRatio, callback) {
     }
     groundShape = new poly2tri.SweepContext(contour);
 
-    //Section2
-    for (var i = 0; i < bldgFootprint.length; i++) {
-        if (bldgFootprint[i][1] < 0) {
-            point[1] = bldgFootprint[i][1] - 5;
-        } else {
-            point[1] = bldgFootprint[i][1] + 5;
+    for (var j = 0; j < distanceRatio; j++) {
+        for (var i = 0; i < bldgFootprint.length; i++) {
+            if (bldgFootprint[i][1] < 0) {
+                point[1] = bldgFootprint[i][1] - j / distanceRatio;
+            } else {
+                point[1] = bldgFootprint[i][1] + j / distanceRatio;
+            }
+            if (bldgFootprint[i][0] < 0) {
+                point[0] = bldgFootprint[i][0] - j / distanceRatio;
+            } else {
+                point[0] = bldgFootprint[i][0] + j / distanceRatio;
+            }
+            groundShape.addPoint(new poly2tri.Point(point[0], point[1]));
         }
-        if (bldgFootprint[i][0] < 0) {
-            point[0] = bldgFootprint[i][0] - 5;
-        } else {
-            point[0] = bldgFootprint[i][0] + 5;
-        }
-        groundShape.addPoint(new poly2tri.Point(point[0], point[1]));
+        console.log(j);
     }
-    for (var i = 0; i < bldgFootprint.length; i++) {
-        if (bldgFootprint[i][1] < 0) {
-            point[1] = bldgFootprint[i][1] - 15;
-        } else {
-            point[1] = bldgFootprint[i][1] + 15;
-        }
-        if (bldgFootprint[i][0] < 0) {
-            point[0] = bldgFootprint[i][0] - 15;
-        } else {
-            point[0] = bldgFootprint[i][0] + 15;
-        }
-        groundShape.addPoint(new poly2tri.Point(point[0], point[1]));
-    }
+
 
     groundShape.triangulate();
     triangles = groundShape.getTriangles();
