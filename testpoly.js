9191ec394ff37826882b896a1ab3e163b0023d81
nmattise <n.mattise@gmail.com>
2014-12-15 15:25:52 -0500

    more loops

--- testpoly.js
+++ testpoly.js
@@ -37,6 +37,19 @@ function createGround(bldgFootprint, distanceRatio, callback) {
         }
         groundShape.addPoint(new poly2tri.Point(point[0], point[1]));
     }
+    for (var i = 0; i < bldgFootprint.length; i++) {
+        if (bldgFootprint[i][1] < 0) {
+            point[1] = bldgFootprint[i][1] - 15;
+        } else {
+            point[1] = bldgFootprint[i][1] + 15;
+        }
+        if (bldgFootprint[i][0] < 0) {
+            point[0] = bldgFootprint[i][0] - 15;
+        } else {
+            point[0] = bldgFootprint[i][0] + 15;
+        }
+        groundShape.addPoint(new poly2tri.Point(point[0], point[1]));
+    }
 
     groundShape.triangulate();
     triangles = groundShape.getTriangles();
