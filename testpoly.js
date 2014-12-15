9955f5df7134bf1326264016c52342a770b24752
nmattise <n.mattise@gmail.com>
2014-12-15 14:46:54 -0500

    poly for ground

--- testpoly.js
+++ testpoly.js
@@ -1,33 +1,38 @@
-var poly2tri = require('poly2tri');
+var fs = require('fs'),
+    stl = require('stl'),
+    poly2tri = require('poly2tri');
+
+
+function createFacet(verts) {
+    return {
+        verts: verts
+    }
+}
 
 var contour = [
-    new poly2tri.Point(0, 0),
-    new poly2tri.Point(10, 0),
-    new poly2tri.Point(10, 10),
-    new poly2tri.Point(0, 10)
-];
-console.log(contour);
+     new poly2tri.Point(100, 100),
+     new poly2tri.Point(100, 300),
+     new poly2tri.Point(300, 300),
+     new poly2tri.Point(300, 100)
+ ];
 var swctx = new poly2tri.SweepContext(contour);
+
+var point = new poly2tri.Point(150, 150);
+swctx.addPoint(point);
 
 swctx.triangulate();
 var triangles = swctx.getTriangles();
-
+var facets = new Array;
 triangles.forEach(function (t) {
+    var verts = [];
     t.getPoints().forEach(function (p) {
-        console.log(p.x, p.y);
+        verts.push([p.x, p.y, 0]);
     });
+    facets.push(createFacet(verts));
 });
-
-function crossProduct(a, b) {
-    // Check lengths
-    if (a.length != 3 || b.length != 3) {
-        return;
-    }
-
-    return [a[1] * b[2] - a[2] * b[1],
-          a[2] * b[0] - a[0] * b[2],
-          a[0] * b[1] - a[1] * b[0]];
-
-}
-
-console.log(crossProduct([0,1,1], [0, 0,1]))
+var stlObj = {
+    description: "ground",
+    facets: facets
+};
+var buildingSTL = stl.fromObject(stlObj);
+fs.writeFileSync("stlFiles/polyGround.stl", buildingSTL);
