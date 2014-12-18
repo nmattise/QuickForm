f276b5d4e2f481669e6bb78e13e77cb48cfd82b5
nmattise <n.mattise@gmail.com>
2014-12-18 10:53:39 -0500

    new Ground

---
+++ newGround.js
@@ -0,0 +1,70 @@
+var fs = require('fs'),
+    stl = require('stl'),
+    poly2tri = require('poly2tri');
+//put verts into verts object for STL
+function createFacet(verts) {
+    return {
+        verts: verts
+    }
+}
+var outerBounds = [
+    [-100, -100],
+    [-100, 100],
+    [100, 100],
+    [100, -100]
+];
+var innerBound = [
+    [-10, -10],
+    [-10, 10],
+    [10, 10],
+    [10, -10]
+];
+var contour = new Array;
+outerBounds.forEach(function (point) {
+    contour.push(new poly2tri.Point(point[0], point[1]));
+});
+
+var ftprint = new poly2tri.SweepContext(contour);
+
+//array stuff
+var innerFullArray = new Array;
+var x = -10;
+var xArr = new Array;
+while (x <= 10) {
+    xArr.push(x);
+    x++;
+}
+var y = -10;
+var yArr = new Array;
+while (y <= 10) {
+    yArr.push(y);
+    y++;
+}
+for (var i = 0; i < xArr.length; i++) {
+    innerFullArray.push(new poly2tri.Point(xArr[i], -10));
+    innerFullArray.push(new poly2tri.Point(xArr[i], 10));
+    innerFullArray.push(new poly2tri.Point(-10, xArr[i]));
+    innerFullArray.push(new poly2tri.Point(10, xArr[i]));
+}
+
+ftprint.addPoints(innerFullArray);
+
+ftprint.triangulate();
+var triangles = ftprint.getTriangles();
+var facets = new Array;
+triangles.forEach(function (tri) {
+    var verts = [];
+    tri.points_.reverse();
+    tri.points_.forEach(function (points) {
+        verts.push([points.x, points.y, 0]);
+    });
+    facets.push(createFacet(verts));
+});
+
+var stlObj = {
+    description: "ground",
+    facets: facets
+};
+
+var buildingSTL = stl.fromObject(stlObj);
+fs.writeFileSync("stlFiles/ground3.stl", buildingSTL);
