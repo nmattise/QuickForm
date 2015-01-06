7455c41556730b18e472cc49db72dbcfcbd6e4c9
nmattise <n.mattise@gmail.com>
2015-01-06 16:58:50 -0500

    roof and floor

--- tb.js
+++ tb.js
@@ -3,7 +3,7 @@
 var fs = require('fs'),
     stl = require('stl');
 
-function createWall(pt1, pt2, z1, z2) {
+function createVertPlane(pt1, pt2, z1, z2) {
     var tri1 = [[pt1[0], pt1[1], z1], [pt2[0], pt2[1], z1], [pt2[0], pt2[1], z2]],
         tri2 = [[pt1[0], pt1[1], z1], [pt2[0], pt2[1], z2], [pt1[0], pt1[1], z2]],
         facets = [];
@@ -16,8 +16,15 @@ function createWall(pt1, pt2, z1, z2) {
 
 }
 
-function createRoofFloor(pt1, pt2) {
-
+function createHorPlane(pt1, pt2, z) {
+    var tri1 = [[pt1[0], pt1[1], 1], [pt2[0], pt2[1], z], [pt2[0], pt2[1], z]],
+        tri2 = [[pt1[0], pt1[1], 1], [pt2[0], pt2[1], z], [pt1[0], pt1[1], z]],
+        facets = [{
+            verts: tri1
+    }, {
+            verts: tri2
+    }];
+    return facets;
 }
 
 var array = [],
@@ -57,23 +64,23 @@ for (var x = 0; x < xGrid; x++) {
         var z2 = zIt * (z + 1);
         var pt1 = [x1, array[0][1]],
             pt2 = [x2, array[0][1]];
-        var tri = createWall(pt1, pt2, z1, z2);
+        var tri = createVertPlane(pt1, pt2, z1, z2);
         facets.push(tri[0]);
         facets.push(tri[1]);
     }
 }
 //North Side
 for (var x = 0; x < xGrid; x++) {
-    var x1 = array[3][0] - xIt * x;
-    var x2 = array[3][0] - xIt * (x + 1);
+    var x1 = array[2][0] - xIt * x;
+    var x2 = array[2][0] - xIt * (x + 1);
     //console.log(array[0][0] - xIt * x)
     for (var z = 0; z < zGrid + 1; z++) {
         //console.log(zIt * z);
         var z1 = zIt * z;
         var z2 = zIt * (z + 1);
-        var pt1 = [x1, array[3][1]],
-            pt2 = [x2, array[3][1]];
-        var tri = createWall(pt1, pt2, z1, z2);
+        var pt1 = [x1, array[2][1]],
+            pt2 = [x2, array[2][1]];
+        var tri = createVertPlane(pt1, pt2, z1, z2);
         facets.push(tri[0]);
         facets.push(tri[1]);
     }
@@ -90,15 +97,15 @@ for (var y = 0; y < yGrid; y++) {
         var z2 = zIt * (z + 1);
         var pt1 = [array[1][0], y1],
             pt2 = [array[1][0], y2];
-        var tri = createWall(pt1, pt2, z1, z2);
+        var tri = createVertPlane(pt1, pt2, z1, z2);
         facets.push(tri[0]);
         facets.push(tri[1]);
     }
 }
 //West Side
 for (var y = 0; y < yGrid; y++) {
-    var y1 = array[3][1] - yIt * y;
-    var y2 = array[3][1] - yIt * (y + 1);
+    var y1 = array[3][1] + yIt * y;
+    var y2 = array[3][1] + yIt * (y + 1);
     //console.log(array[0][0] - xIt * x)
     for (var z = 0; z < zGrid + 1; z++) {
         //console.log(zIt * z);
@@ -106,11 +113,31 @@ for (var y = 0; y < yGrid; y++) {
         var z2 = zIt * (z + 1);
         var pt1 = [array[3][0], y1],
             pt2 = [array[3][0], y2];
-        var tri = createWall(pt1, pt2, z1, z2);
+        var tri = createVertPlane(pt1, pt2, z1, z2);
         facets.push(tri[0]);
         facets.push(tri[1]);
     }
 }
+//Roof & Floor
+for (var x = 0; x < xGrid; x++) {
+    var x1 = array[0][0] - xIt * x;
+    var x2 = array[0][0] - xIt * (x + 1);
+    //console.log(array[0][0] - xIt * x)
+    for (var y = 0; y < yGrid; y++) {
+        //console.log(zIt * z);
+        var y1 = array[1][1] - yIt * y;
+        var y2 = array[1][1] - yIt * (y + 1);
+        var pt1 = [x1, y1],
+            pt2 = [x2, y2];
+        var tri = createHorPlane(pt1, pt2, 0);
+        facets.push(tri[0]);
+        facets.push(tri[1]);
+        var tri = createHorPlane(pt1, pt2, height);
+        facets.push(tri[0]);
+        facets.push(tri[1]);
+    }
+}
+
 var stlObj = {
     description: "testBuilding",
     facets: facets
