42e14fd2b038f4b5166ea928b08c8cb15b43e82b
nmattise <n.mattise@gmail.com>
2015-01-07 17:21:37 -0500

    ready to build

--- tb.js
+++ tb.js
@@ -42,6 +42,31 @@ function createHorPlaneDn(pt1, pt2, z) {
     return facets;
 }
 
+function createWallGrid(point1, point2, height) {
+    var sideLength, deltaX, deltaY, gridLength, xIt, yIt, iterator, pt1, pt2, i, z, zGrid, zIt, tri, facets, z1, z2;
+    facets = [];
+    sideLength = distanceFormula(point2[0], point2[1], point1[0], point1[1]);
+    deltaX = point2[0] - point1[0];
+    deltaY = point2[1] - point1[1];
+    gridLength = sideLength / parseInt(sideLength);
+    xIt = deltaX / parseInt(sideLength);
+    yIt = deltaY / parseInt(sideLength);
+    iterator = parseInt(sideLength);
+    zGrid = Math.abs(parseInt(height));
+    zIt = height / zGrid;
+    for (i = 0; i < sideLength - 1; i++) {
+        pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
+        pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
+        for (z = 0; z < zGrid; z++) {
+            z1 = zIt * z;
+            z2 = zIt * (z + 1);
+            tri = createVertPlane(pt1, pt2, z1, z2);
+            facets.push(tri[0]);
+            facets.push(tri[1]);
+        }
+    }
+    return facets;
+}
 var array = [],
     height,
     xGrid,
@@ -60,286 +85,20 @@ array = [[0, -10],
     [-10, 0]];
 height = 10;
 
-/*xGrid = Math.abs(parseInt(array[0][0] - array[1][0], 10));
-xIt = (array[0][0] - array[1][0]) / xGrid;
-console.log("xGrid: " + xGrid + "  X iteration: " + xIt);
-yGrid = Math.abs(parseInt(array[0][1] - array[2][1], 10));
-yIt = (array[0][1] - array[2][1]) / yGrid;
-console.log("yGrid: " + yGrid + "  Y iteration: " + yIt);
-zGrid = Math.abs(parseInt(height));
-zIt = height / zGrid;
-console.log("zGrid: " + zGrid + "  Z iteration: " + zIt);
-
-//South Side
-for (var x = 0; x < xGrid; x++) {
-    var x1 = array[0][0] - xIt * x;
-    var x2 = array[0][0] - xIt * (x + 1);
-    //console.log(array[0][0] - xIt * x)
-    for (var z = 0; z < zGrid; z++) {
-        //console.log(zIt * z);
-        var z1 = zIt * z;
-        var z2 = zIt * (z + 1);
-        var pt1 = [x1, array[0][1]],
-            pt2 = [x2, array[0][1]];
-        var tri = createVertPlane(pt1, pt2, z1, z2);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-}
-//North Side
-for (var x = 0; x < xGrid; x++) {
-    var x1 = array[2][0] + xIt * x;
-    var x2 = array[2][0] + xIt * (x + 1);
-    //console.log(array[0][0] - xIt * x)
-    for (var z = 0; z < zGrid; z++) {
-        //console.log(zIt * z);
-        var z1 = zIt * z;
-        var z2 = zIt * (z + 1);
-        var pt1 = [x1, array[2][1]],
-            pt2 = [x2, array[2][1]];
-        var tri = createVertPlane(pt1, pt2, z1, z2);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-}
-
-//East Side
-for (var y = 0; y < yGrid; y++) {
-    var y1 = array[1][1] - yIt * y;
-    var y2 = array[1][1] - yIt * (y + 1);
-    //console.log(array[0][0] - xIt * x)
-    for (var z = 0; z < zGrid; z++) {
-        //console.log(zIt * z);
-        var z1 = zIt * z;
-        var z2 = zIt * (z + 1);
-        var pt1 = [array[1][0], y1],
-            pt2 = [array[1][0], y2];
-        var tri = createVertPlane(pt1, pt2, z1, z2);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-}
-//West Side
-for (var y = 0; y < yGrid; y++) {
-    var y1 = array[3][1] + yIt * y;
-    var y2 = array[3][1] + yIt * (y + 1);
-    //console.log(array[0][0] - xIt * x)
-    for (var z = 0; z < zGrid; z++) {
-        //console.log(zIt * z);
-        var z1 = zIt * z;
-        var z2 = zIt * (z + 1);
-        var pt1 = [array[3][0], y1],
-            pt2 = [array[3][0], y2];
-        var tri = createVertPlane(pt1, pt2, z1, z2);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-}
-//Roof & Floor
-for (var x = 0; x < xGrid; x++) {
-    var x1 = array[0][0] - xIt * x;
-    var x2 = array[0][0] - xIt * (x + 1);
-    //console.log(array[0][0] - xIt * x)
-    for (var y = 0; y < yGrid; y++) {
-        //console.log(zIt * z);
-        var y1 = array[0][1] - yIt * y;
-        var y2 = array[0][1] - yIt * (y + 1);
-        var pt1 = [x1, y1],
-            pt2 = [x2, y2];
-        //Floor
-        var tri = createHorPlaneDn(pt1, pt2, 0);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-        //Roof
-        var tri = createHorPlaneUp(pt1, pt2, height);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-}
-
-var stlObj = {
-    description: "testBuilding",
-    facets: facets
-};
-fs.writeFileSync("stlFiles/testBuildings.stl", stl.fromObject(stlObj));*/
 
 
-//Loop that is a function of x,y,z
-/*for (var i = 1; i < array.length; i++) {
-    var tri = createVertPlane(array[i - 1], array[i], 0, height);
-    facets.push(tri[0]);
-    facets.push(tri[1]);
-}
-var tri = createVertPlane(array[array.length - 1], array[0], 0, height);
-facets1.push(tri[0]);
-facets1.push(tri[1]);*/
-
-//Side 1
-//Get grid# and grid iterators in x,y,z
-/*var sideLength = distanceFormula(array[0][0], array[0][1], array[1][0], array[1][1]);
-var deltaX = array[1][0] - array[0][0];
-var deltaY = array[1][1] - array[0][1];
-
-var gridLength = sideLength / parseInt(sideLength);
-var xIt = deltaX / parseInt(sideLength);
-var yIt = deltaY / parseInt(sideLength);
-var iterator = parseInt(sideLength);
-console.log("sidelength: " + sideLength);
-console.log("deltax: " + deltaX);
-console.log("deltay: " + deltaY);
-console.log("gridLength: " + gridLength);
-console.log("xIterator: " + xIt);
-console.log("yIterator: " + yIt);
-console.log("Iterator: " + iterator);
-
-for (var i = 0; i < sideLength - 1; i++) {
-    var pt1 = [array[0][0] + (xIt * i), array[0][1] + (yIt * i)];
-    var pt2 = [array[0][0] + (xIt * (i + 1)), array[0][1] + (yIt * (i + 1))];
-    console.log("pt1: " + pt1 + "  pt2: " + pt2);
-    var tri = createVertPlane(pt1, pt2, 0, height);
-    facets.push(tri[0]);
-    facets.push(tri[1]);
-}*/
-//Loop Through Sides
-/*var zGrid = Math.abs(parseInt(height));
-var zIt = height / zGrid;
-for (var i = 1; i <= 2; i++) {
-    var sideLength = distanceFormula(array[i - 1][0], array[i - 1][1], array[i][0], array[i][1]),
-        deltaX = array[i][0] - array[i - 1][0],
-        deltaY = array[i][1] - array[i - 1][1],
-        gridLength = sideLength / parseInt(sideLength),
-        xIt = deltaX / parseInt(sideLength),
-        yIt = deltaY / parseInt(sideLength),
-        iterator = parseInt(sideLength);
-    console.log("sidelength: " + sideLength);
-    console.log("deltax: " + deltaX);
-    console.log("deltay: " + deltaY);
-    console.log("gridLength: " + gridLength);
-    console.log("xIterator: " + xIt);
-    console.log("yIterator: " + yIt);
-    console.log("Iterator: " + iterator);
-    console.log("zGrid: " + zGrid + "  Z iteration: " + zIt);
-    for (var j = 0; j < sideLength - 1; j++) {
-        var pt1 = [array[i - 1][0] + (xIt * j), array[i - 1][1] + (yIt * j)];
-        var pt2 = [array[i - 1][0] + (xIt * (j + 1)), array[i - 1][1] + (yIt * (j + 1))];
-        console.log("pt1: " + pt1 + "  pt2: " + pt2);
-        for (var z = 0; z < zGrid; z++) {
-            var z1 = zIt * z;
-            var z2 = zIt * (z + 1);
-            var tri = createVertPlane(pt1, pt2, z1, z2);
-            facets.push(tri[0]);
-            facets.push(tri[1]);
-        }
-
-    }
-}
-for (var i = 3; i < array.length; i++) {
-    var sideLength = distanceFormula(array[i - 1][0], array[i - 1][1], array[i][0], array[i][1]),
-        deltaX = array[i][0] - array[i - 1][0],
-        deltaY = array[i][1] - array[i - 1][1],
-        gridLength = sideLength / parseInt(sideLength),
-        xIt = deltaX / parseInt(sideLength),
-        yIt = deltaY / parseInt(sideLength),
-        iterator = parseInt(sideLength);
-    console.log("sidelength: " + sideLength);
-    console.log("deltax: " + deltaX);
-    console.log("deltay: " + deltaY);
-    console.log("gridLength: " + gridLength);
-    console.log("xIterator: " + xIt);
-    console.log("yIterator: " + yIt);
-    console.log("Iterator: " + iterator);
-
-    console.log("zGrid: " + zGrid + "  Z iteration: " + zIt);
-    for (var j = 0; j < sideLength - 1; j++) {
-        var pt1 = [array[i - 1][0] + (xIt * j), array[i - 1][1] + (yIt * j)];
-        var pt2 = [array[i - 1][0] + (xIt * (j + 1)), array[i - 1][1] + (yIt * (j + 1))];
-        console.log("pt1: " + pt1 + "  pt2: " + pt2);
-        for (var z = 0; z < zGrid; z++) {
-            var z1 = zIt * z;
-            var z2 = zIt * (z + 1);
-            var tri = createVertPlane(pt1, pt2, z1, z2);
-            facets.push(tri[0]);
-            facets.push(tri[1]);
-        }
-
-    }
-}
-
-
-var sideLength = distanceFormula(array[3][0], array[3][1], array[0][0], array[0][1]),
-    deltaX = array[0][0] - array[3][0],
-    deltaY = array[0][1] - array[3][1],
-    gridLength = sideLength / parseInt(sideLength),
-    xIt = deltaX / parseInt(sideLength),
-    yIt = deltaY / parseInt(sideLength),
-    iterator = parseInt(sideLength);
-console.log("sidelength: " + sideLength);
-console.log("deltax: " + deltaX);
-console.log("deltay: " + deltaY);
-console.log("gridLength: " + gridLength);
-console.log("xIterator: " + xIt);
-console.log("yIterator: " + yIt);
-console.log("Iterator: " + iterator);
-
-console.log("zGrid: " + zGrid + "  Z iteration: " + zIt);
-for (var j = 0; j < sideLength - 1; j++) {
-    var pt1 = [array[3][0] + (xIt * j), array[3][1] + (yIt * j)];
-    var pt2 = [array[3][0] + (xIt * (j + 1)), array[3][1] + (yIt * (j + 1))];
-    console.log("pt1: " + pt1 + "  pt2: " + pt2);
-    for (var z = 0; z < zGrid; z++) {
-        var z1 = zIt * z;
-        var z2 = zIt * (z + 1);
-        var tri = createVertPlane(pt1, pt2, z1, z2);
-        facets.push(tri[0]);
-        facets.push(tri[1]);
-    }
-
-}*/
-
-
-function createWallGrid(point1, point2, height) {
-    var sideLength, deltaX, deltaY, gridLength, xIt, yIt, iterator, pt1, pt2, i, z, zGrid, zIt, tri, facets, z1, z2;
-    facets = [];
-    sideLength = distanceFormula(point2[0], point2[1], point1[0], point1[1]);
-    deltaX = point2[0] - point1[0];
-    deltaY = point2[1] - point1[1];
-    gridLength = sideLength / parseInt(sideLength);
-    xIt = deltaX / parseInt(sideLength);
-    yIt = deltaY / parseInt(sideLength);
-    iterator = parseInt(sideLength);
-    zGrid = Math.abs(parseInt(height));
-    zIt = height / zGrid;
-    for (i = 0; i < sideLength; i++) {
-        pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
-        pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
-        for (z = 0; z < zGrid; z++) {
-            z1 = zIt * z;
-            z2 = zIt * (z + 1);
-            tri = createVertPlane(pt1, pt2, z1, z2);
-            facets.push(tri[0]);
-            facets.push(tri[1]);
-        }
-    }
-    return facets;
-}
-
-var wall = createWallGrid(array[0], array[1], height);
-wall.forEach(function (facet) {
+createWallGrid(array[0], array[1], height).forEach(function (facet) {
     facets.push(facet);
 });
-var wall = (createWallGrid(array[1], array[2], height));
-wall.forEach(function (facet) {
+(createWallGrid(array[1], array[2], height)).forEach(function (facet) {
     facets.push(facet);
 });
-var wall = (createWallGrid(array[2], array[3], height));
-wall.forEach(function (facet) {
+(createWallGrid(array[2], array[3], height)).forEach(function (facet) {
     facets.push(facet);
 });
-var wall = (createWallGrid(array[3], array[0], height));
-wall.forEach(function (facet) {
+(createWallGrid(array[3], array[0], height)).forEach(function (facet) {
     facets.push(facet);
 });
-console.log(facets);
 var stlObj1 = {
     description: "testBuilding1",
     facets: facets
