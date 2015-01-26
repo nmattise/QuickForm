130bf08a2cd2c771aa888f838f81fe78e3fbe784
nmattise <n.mattise@gmail.com>
2015-01-26 16:05:08 -0500

    material description in facet

--- materials.js
+++ materials.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
     poly2tri = require('poly2tri');
 
 
-function createVertPlane(pt1, pt2, z1, z2) {
+function createVertPlane(pt1, pt2, z1, z2, material) {
     var tri1 = [
             [pt1[0], pt1[1], z1],
             [pt2[0], pt2[1], z1],
@@ -20,9 +20,11 @@ function createVertPlane(pt1, pt2, z1, z2) {
         ],
         facets = [];
     facets = [{
-        verts: tri1
+        verts: tri1,
+        material: material
     }, {
-        verts: tri2
+        verts: tri2,
+        material: material
     }];
     return facets;
 
@@ -57,17 +59,13 @@ function material(point1, point2, height, floorHeight, floors, windowWallRatio, 
         var floorTop = floor + floorHeight;
         var sillBottom1 = sillBottom + (t * floorHeight);
         var sillTop1 = sillTop + (t * floorHeight);
-        console.log(floor);
-        console.log(floorTop);
-    	console.log(sillBottom1);
-    	console.log(sillTop1);
         for (i = 0; i < iterator; i++) {
             pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
             pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
 
-            tri = createVertPlane(pt1, pt2, floor, sillBottom1);
-            tri1 = createVertPlane(pt1, pt2, sillBottom1, sillTop1);
-            tri2 = createVertPlane(pt1, pt2, sillTop1, floorTop);
+            tri = createVertPlane(pt1, pt2, floor, sillBottom1, "wall");
+            tri1 = createVertPlane(pt1, pt2, sillBottom1, sillTop1, "window");
+            tri2 = createVertPlane(pt1, pt2, sillTop1, floorTop, "wall");
             facets.push(tri[0]);
             facets.push(tri[1]);
             facets.push(tri1[0]);
@@ -77,7 +75,6 @@ function material(point1, point2, height, floorHeight, floors, windowWallRatio, 
         }
 
     };
-  
     var stlObj = {
         description: buildingName,
         facets: facets
