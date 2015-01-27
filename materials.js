0436b907dcab29037e50430e9a5141c725ae7fb0
nmattise <n.mattise@gmail.com>
2015-01-27 15:25:46 -0500

    basic material.js setup

--- materials.js
+++ materials.js
@@ -1,10 +1,6 @@
-//Polygon Triangulation (for top/bottom)
-//http://polyk.ivank.net/?p=documentation
-//https://github.com/r3mi/poly2tri.js
 
 var fs = require('fs'),
-    stl = require('stl'),
-    poly2tri = require('poly2tri');
+    stl = require('stl');
 
 
 function createVertPlane(pt1, pt2, z1, z2, material) {
@@ -55,88 +51,46 @@ function material(point1, point2, height, floorHeight, floors, windowWallRatio, 
     iterator = parseInt(sideLength / gridSize);
 
     for (var t = 0; t < floors; t++) {
-        var z0 = t * floorHeight,
-            z1 = z0 + floorHeight,
-            w0 = sillBottom + (t * floorHeight),
-            w1 = sillTop + (t * floorHeight);
+        var floor = t * floorHeight;
+        var floorTop = floor + floorHeight;
+        var sillBottom1 = sillBottom + (t * floorHeight);
+        var sillTop1 = sillTop + (t * floorHeight);
+        for (i = 0; i < iterator; i++) {
+            pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
+            pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
 
-        if (gridSize > (2 * (w1 - w0))) {
-            var wHeight = w1 - w0;
-            var wGrid = ((wHeight % gridSize) / (parseInt(wHeight / gridSize))) + gridSize;
-            var wIt = wHeight / parseInt(wHeight / gridSize);
-            var wIterator = parseInt(wHeight / gridSize);
-            for (i = 0; i < iterator; i++) {
-                pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
-                pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
-                for (var l = 0; l < wIterator; l++) {
-                    win0 = w0 + (wIt * l);
-                    win1 = w0 + (wIt * (l + 1));
-                    tri1 = createVertPlane(pt1, pt2, win0, win1, "glass");
-                    facets.push(tri1[0]);
-                    facets.push(tri1[1]);
-                }
-
-            }
-        } else {
-            for (i = 0; i < iterator; i++) {
-                pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
-                pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
-                tri1 = createVertPlane(pt1, pt2, w0, w1, "glass");
-                facets.push(tri1[0]);
-                facets.push(tri1[1]);
-            }
+            tri = createVertPlane(pt1, pt2, floor, sillBottom1, "wall");
+            tri1 = createVertPlane(pt1, pt2, sillBottom1, sillTop1, "window");
+            tri2 = createVertPlane(pt1, pt2, sillTop1, floorTop, "wall");
+            facets.push(tri[0]);
+            facets.push(tri[1]);
+            facets.push(tri1[0]);
+            facets.push(tri1[1]);
+            facets.push(tri2[0]);
+            facets.push(tri2[1]);
         }
-        if (gridSize > (2 * (z1 - z0))) {
-            var zHeight = z1 - z0;
-            var zGrid = ((zHeight % gridSize) / (parseInt(zHeight / gridSize))) + gridSize;
-            var zIt = zHeight / parseInt(zHeight / gridSize);
-            var zIterator = parseInt(zHeight / gridSize);
-            for (i = 0; i < iterator; i++) {
-                pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
-                pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
-                for (var l = 0; l < wIterator; l++) {
-                    wall0 = z0 + (zIt * l);
-                    wall1 = z0 + (zIt * (l + 1));
-                    wall2 = w1 + (zIt * l);
-                    wall3 = w1 + (zIt * (l + 1));
-                    tri = createVertPlane(pt1, pt2, wall0, wall1, "brick");
-                    tri2 = createVertPlane(pt1, pt2, wall2, wall3, "brick");
-                    facets.push(tri[0]);
-                    facets.push(tri[1]);
-                    facets.push(tri2[0]);
-                    facets.push(tri2[1]);
-                }
-
-            }
-        } else {
-            for (i = 0; i < iterator; i++) {
-                pt1 = [point1[0] + (xIt * i), point1[1] + (yIt * i)];
-                pt2 = [point1[0] + (xIt * (i + 1)), point1[1] + (yIt * (i + 1))];
-                tri = createVertPlane(pt1, pt2, z0, w0, "brick");
-                tri2 = createVertPlane(pt1, pt2, w1, z1, "brick");
-                facets.push(tri[0]);
-                facets.push(tri[1]);
-                facets.push(tri2[0]);
-                facets.push(tri2[1]);
-            }
-        }
-
 
     };
     return facets;
+    /*var stlObj = {
+        description: buildingName,
+        facets: facets
+    };
+    fs.writeFileSync("stlFiles/materialRectangle.stl", stl.fromObject(stlObj));*/
+
 }
 
 var facets = [];
-material([-10, -10], [10, -10], 20, 3.25, 4, .4, 1, "materialRectangle").forEach(function(facet) {
+material([-10, -10], [10, -10], 20, 5, 4, .25, 3, "materialRectangle").forEach(function(facet) {
     facets.push(facet)
 });
-material([10, -10], [10, 10], 20, 3.25, 4, .4, 1, "materialRectangle").forEach(function(facet) {
+material([10, -10], [10, 10], 20, 5, 4, .25, 3, "materialRectangle").forEach(function(facet) {
     facets.push(facet)
 });
-material([10, 10], [-10, 10], 20, 3.25, 4, .4, 1, "materialRectangle").forEach(function(facet) {
+material([10, 10], [-10, 10], 20, 5, 4, .25, 3, "materialRectangle").forEach(function(facet) {
     facets.push(facet)
 });
-material([-10, 10], [-10, -10], 20, 3.25, 4, .4, 1, "materialRectangle").forEach(function(facet) {
+material([-10, 10], [-10, -10], 20, 5, 4, .25, 3, "materialRectangle").forEach(function(facet) {
     facets.push(facet)
 });
 
