ad82ecf38be522d3e1cdd97a26daa09d7c0bda93
nmattise <n.mattise@gmail.com>
2015-01-28 18:22:09 -0500

    rotate polygon and more shapes on client side and H shape tests on server

--- public/js/coord.js
+++ public/js/coord.js
@@ -1,13 +1,13 @@
 //LatLon Stuff
-Number.prototype.toRadians = function () {
+Number.prototype.toRadians = function() {
     return this * Math.PI / 180;
 };
-Number.prototype.toDegrees = function () {
+Number.prototype.toDegrees = function() {
     return this * 180 / Math.PI;
 };
 
 //Prototype to find side length of array of points
-Array.prototype.findLengths = function () {
+Array.prototype.findLengths = function() {
     var sideLengths = new Array;
     for (var i = 1; i < this.length; i++) {
         var d = distanceFormula(this[i - 1][0], this[i - 1][1], this[i][0], this[i][1]);
@@ -21,6 +21,7 @@ Array.prototype.findLengths = function () {
 function distanceFormula(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 }
+
 function polygonArea(X, Y, numPoints) {
     area = 0; // Accumulates area in the loop
     j = numPoints - 1; // The last vertex is the 'previous' one to the first
@@ -32,13 +33,15 @@ function polygonArea(X, Y, numPoints) {
     return area / 2;
 }
 
+//http://www.movable-type.co.uk/scripts/latlong.html
 //Create Lat/Lng object
 function latLon(lat, lng) {
     this.latitude = Number(lat);
     this.longitude = Number(lng);
 }
+
 //Latitude and Longitude to X,Y Coord
-latLon.prototype.coordinatesTo = function (point) {
+latLon.prototype.coordinatesTo = function(point) {
     var radius = 6371;
     var phi1 = this.latitude.toRadians(),
         lambda1 = this.longitude.toRadians(),
@@ -60,26 +63,148 @@ latLon.prototype.coordinatesTo = function (point) {
     return [X, Y];
 }
 
-//Coordinates from Basic Lat/lng point to Lat/Lng
+/**
+ * Returns the distance from 'this' point to destination point (using haversine formula).
+ *
+ * @param   {LatLon} point - Latitude/longitude of destination point.
+ * @returns {number} Distance between this point and destination point, in km (on sphere of 'this' radius).
+ *
+ * @example
+ *     var p1 = new LatLon(52.205, 0.119), p2 = new LatLon(48.857, 2.351);
+ *     var d = p1.distanceTo(p2); // d.toPrecision(4): 404.3
+ */
+latLon.prototype.distanceTo = function(point) {
+    var R = 6371;
+    var phi1 = this.latitude.toRadians(),
+        lambda1 = this.longitude.toRadians();
+    var phi2 = point.latitude.toRadians(),
+        lambda2 = point.longitude.toRadians();
+    var deltaPhi = phi2 - phi1;
+    var deltaLambda = lambda2 - lambda1;
 
-latLon.prototype.destinationPoint = function (X, Y) {
-    var radius = 6371;
-    var brng = Math.atan(X / Y),
-        dist = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2));
+    var a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c * 1000;
+    return d;
+};
+
+
+/**
+ * Returns the (initial) bearing from 'this' point to destination point.
+ *
+ * @param   {LatLon} point - Latitude/longitude of destination point.
+ * @returns {number} Initial bearing in degrees from north.
+ *
+ * @example
+ *     var p1 = new LatLon(52.205, 0.119), p2 = new LatLon(48.857, 2.351);
+ *     var b1 = p1.bearingTo(p2); // b1.toFixed(1): 156.2
+ */
+latLon.prototype.bearingTo = function(point) {
+    var phi1 = this.latitude.toRadians(),
+        phi2 = point.latitude.toRadians();
+    var deltaLambda = (point.longitude - this.longitude).toRadians();
+
+    // see http://mathforum.org/library/drmath/view/55417.html
+    var y = Math.sin(deltaLambda) * Math.cos(phi2);
+    var x = Math.cos(phi1) * Math.sin(phi2) - Math.sin(phi1) * Math.cos(phi2) * Math.cos(deltaLambda);
+    var theta = Math.atan2(y, x);
+    return (theta.toDegrees() + 360) % 360;
+};
+
+
+//@example
+//      var p1 = new LatLon(51.4778, -0.0015);
+//     var p2 = p1.destinationPoint(300.7, 7.794); // p2.toString(): 51.5135°N, 000.0983°W
+
+latLon.prototype.destinationPoint = function(brng, dist) {
+    // see http://williams.best.vwh.net/avform.htm#LL
+
     var theta = Number(brng).toRadians();
-    var delta = Number(dist) / radius; // angular distance in radians
+    var delta = Number(dist) / 6371; // angular distance in radians
 
     var phi1 = this.latitude.toRadians();
     var lambda1 = this.longitude.toRadians();
 
-    var phi2 = Math.asin(Math.sin(phi1) * Math.cos(delta) +
-        Math.cos(phi1) * Math.sin(delta) * Math.cos(theta));
-    var lambda2 = lambda1 + Math.atan2(Math.sin(theta) * Math.sin(delta) * Math.cos(phi1),
-        Math.cos(delta) - Math.sin(phi1) * Math.sin(phi2));
-    lambda2 = (lambda2 + 3 * Math.PI) % (2 * Math.PI) - Math.PI; // normalise to -180..+180º
+    var phi2 = Math.asin(Math.sin(phi1) * Math.cos(delta) + Math.cos(phi1) * Math.sin(delta) * Math.cos(theta));
+    var lambda2 = lambda1 + Math.atan2(Math.sin(theta) * Math.sin(delta) * Math.cos(phi1), Math.cos(delta) - Math.sin(phi1) * Math.sin(phi2));
+    lambda2 = (lambda2 + 3 * Math.PI) % (2 * Math.PI) - Math.PI; // normalise to -180..+180°
 
     return new latLon(phi2.toDegrees(), lambda2.toDegrees());
+};
+
+//Rotate Functions
+//Adjust Rectangle
+function rotatePoint(startPoint, point, theta) {
+    var rotatedPoint, x1, y1;
+    var x0 = startPoint[0],
+        y0 = startPoint[1],
+        x = point[0],
+        y = point[1];
+    x1 = x0 + (x - x0) * Math.cos(theta) + (y - y0) * Math.sin(theta);
+    y1 = y0 - (x - x0) * Math.sin(theta) + (y - y0) * Math.cos(theta);
+    rotatedPoint = [x1, y1];
+    return rotatedPoint;
 }
+
+function findRotation(pt1, pt2) {
+    var deltaX, deltaY, theta;
+    deltaX = pt2[0] - pt1[0];
+    deltaY = pt2[1] - pt1[1];
+    theta = Math.atan(deltaX / deltaY);
+    return theta
+}
+
+//Polygon
+//http://stackoverflow.com/questions/16282330/find-centerpoint-of-polygon-in-javascript
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function Region(points) {
+    this.points = points || [];
+    this.length = points.length;
+}
+
+Region.prototype.area = function() {
+    var area = 0,
+        i,
+        j,
+        point1,
+        point2;
+
+    for (i = 0, j = this.length - 1; i < this.length; j = i, i++) {
+        point1 = this.points[i];
+        point2 = this.points[j];
+        area += point1.x * point2.y;
+        area -= point1.y * point2.x;
+    }
+    area /= 2;
+
+    return area;
+};
+
+Region.prototype.centroid = function() {
+    var x = 0,
+        y = 0,
+        i,
+        j,
+        f,
+        point1,
+        point2;
+
+    for (i = 0, j = this.length - 1; i < this.length; j = i, i++) {
+        point1 = this.points[i];
+        point2 = this.points[j];
+        f = point1.x * point2.y - point2.x * point1.y;
+        x += (point1.x + point2.x) * f;
+        y += (point1.y + point2.y) * f;
+    }
+
+    f = this.area() * 6;
+
+    return new Point(x / f, y / f);
+};
 
 function getArea(latLngPath) {
     //initiallize Variables
@@ -98,7 +223,7 @@ function getArea(latLngPath) {
     avgLng = 0;
     count = 0;
     //Find Origin Point, average of Lat and Lng
-    latLngPath.forEach(function (pt) {
+    latLngPath.forEach(function(pt) {
         avgLat += pt.latitude;
         avgLng += pt.longitude;
         count++;
@@ -107,13 +232,13 @@ function getArea(latLngPath) {
     avgLng = avgLng / count;
     origin = new latLon(avgLat, avgLng);
     //Create Cartesian Coordinates in SI Units
-    latLngPath.forEach(function (pt) {
+    latLngPath.forEach(function(pt) {
         point = new latLon(pt.latitude, pt.longitude);
         coords.push(origin.coordinatesTo(point));
     });
     //Find area of polygon
     sides = coords.length;
-    coords.forEach(function (pt) {
+    coords.forEach(function(pt) {
         x.push(pt[0]);
         y.push(pt[1]);
     });
