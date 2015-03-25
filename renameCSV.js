79e2870c5ebbe5efba00a4ae1af07ef8777a0b67
nmattise <n.mattise@gmail.com>
2015-03-25 17:28:52 -0400

    basic naming, no roof grid

--- renameCSV.js
+++ renameCSV.js
@@ -4,13 +4,24 @@ var csv = require('csv');
 
 var csvData = fs.readFileSync('./Output/LShape.csv', 'utf8');
 
+Array.prototype.clean = function(deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
+            this.splice(i, 1);
+            i--;
+        }
+    }
+    return this;
+};
 
 csv.parse(csvData, function(err, data) {
+    if (err) throw err;
+    //Create Organized Data Array
     var organizedData = new Array(data.length);
     for (var i = 0; i < organizedData.length; i++) {
         organizedData[i] = [];
     };
-    if (err) throw err;
+    //Delete Surfaces containing subsurfaces and rename subsurfac
     for (var i = 0; i <= data[0].length - 1; i++) {
         if (data[0][i].indexOf('SUB') >= 0) {
             data[0][i] = data[0][i - 1];
@@ -19,6 +30,8 @@ csv.parse(csvData, function(err, data) {
             });
         };
     };
+
+    //Organize Data
     for (var j = 1; j < data[0].length; j++) {
         if (data[0][j]) {
             var id = data[0][j].split(' ')[1].split(':')[0];
@@ -27,6 +40,15 @@ csv.parse(csvData, function(err, data) {
             }
         };
     };
+    //Remove Undefined Columns
+    for (var j = 0; j < organizedData.length; j++) {
+        organizedData[j].clean(undefined);
+    }
+    //Rename Columns based upon location
+    for (var j = 0; j < organizedData[0].length; j++) {
+        organizedData[0][j] = j + 1;
+    }
+
     console.log(organizedData[0]);
     csv.stringify(organizedData, function(err, output) {
         fs.writeFileSync('./Output/LShape_new.csv', output);
